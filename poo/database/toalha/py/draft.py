689d701e5dbe5b21137ca9a196c1179abd3106bc
Luis Maia <toyahofficial@gmail.com>
2025-09-17 15:02:39 +0000

    pupupoo

---
+++ poo/database/toalha/py/draft.py
@@ -0,0 +1,12 @@
+class Towel:
+    def __init__(self,color:str,size:str):
+        self.color:str=color
+        self.size:str=size
+        self.wetness:int=0
+
+
+print("Qual a cor da sua toalha?")
+color=input()
+size=input()
+towel:Towel=Towel(color,size)
+print(f"Sua toalha é {towel.color} e {towel.size}")
